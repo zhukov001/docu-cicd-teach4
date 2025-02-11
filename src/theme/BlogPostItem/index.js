f8e59b6a06ab05dfa43313d1ce0dba1e942747a7
zhukov001 <i.zhukov@web.de>
2025-02-11 18:04:37 +0100

    Update index.js

--- src/theme/BlogPostItem/index.js
+++ src/theme/BlogPostItem/index.js
@@ -2,9 +2,18 @@ import React from 'react';
 import BlogPostItem from '@theme-original/BlogPostItem';
 
 export default function BlogPostItemWrapper(props) {
+  const { metadata, isBlogPostPage } = useBlogPost()
+  const isBrowser = useIsBrowser();
+
+  const { frontMatter, slug, title } = metadata
+  const { enableComments } = frontMatter
+
   return (
     <>
       <BlogPostItem {...props} />
+      {(enableComments && isBlogPostPage) && (
+        <GiscusComponent />
+      )}
     </>
   );
 }
