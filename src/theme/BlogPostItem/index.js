c8978dd17195e41a381cd1fdecc641bcfc9401b5
Ilya Zhukov <i.zhukov@fz-juelich.de>
2025-02-11 18:33:34 +0100

    Add option to add comments

--- src/theme/BlogPostItem/index.js
+++ src/theme/BlogPostItem/index.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import BlogPostItem from '@theme-original/BlogPostItem';
+import GiscusComponent from '@site/src/components/GiscusComponent';
+import useIsBrowser from '@docusaurus/useIsBrowser';
 
 export default function BlogPostItemWrapper(props) {
   return (
