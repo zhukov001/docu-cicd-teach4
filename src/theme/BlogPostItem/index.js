8dcec1e7cd09f9e773769c0afd29f8a3bc0a5b72
Ilya Zhukov <i.zhukov@fz-juelich.de>
2025-02-11 18:35:30 +0100

    Add option to add comments

--- src/theme/BlogPostItem/index.js
+++ src/theme/BlogPostItem/index.js
@@ -4,9 +4,17 @@ import GiscusComponent from '@site/src/components/GiscusComponent';
 import useIsBrowser from '@docusaurus/useIsBrowser';
 
 export default function BlogPostItemWrapper(props) {
+  const { metadata, isBlogPostPage } = useBlogPost()
+  const isBrowser = useIsBrowser();
+
+  const { frontMatter, slug, title } = metadata
+  const { enableComments } = frontMatter
   return (
     <>
       <BlogPostItem {...props} />
+      {(enableComments && isBlogPostPage) && (
+        <GiscusComponent />
+      )}
     </>
   );
 }
