f148bc4b36b5e7dc1d5693e3293531e395d9d870
zhukov001 <i.zhukov@web.de>
2025-02-11 18:07:47 +0100

    Update index.js

--- src/theme/BlogPostItem/index.js
+++ src/theme/BlogPostItem/index.js
@@ -1,5 +1,8 @@
 import React from 'react';
+import { useBlogPost } from '@docusaurus/theme-common/internal'
 import BlogPostItem from '@theme-original/BlogPostItem';
+import GiscusComponent from '@site/src/components/GiscusComponent';
+import useIsBrowser from '@docusaurus/useIsBrowser';
 
 export default function BlogPostItemWrapper(props) {
   const { metadata, isBlogPostPage } = useBlogPost()
