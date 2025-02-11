8d5bdc1c9b1c1f5db697094d8d4e51603c0dbd96
Ilya Zhukov <i.zhukov@fz-juelich.de>
2025-02-11 22:22:50 +0100

    Add option to add comments

--- src/theme/DocPaginator/index.tsx
+++ src/theme/DocPaginator/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import DocPaginator from '@theme-original/DocPaginator';
-import {CommentSection} from '@site/src/components/Mdx';
+import {CommentSection} from '@site/src/components/CommentSection';
 
 export default function DocPaginatorWrapper(props) {
     return (
